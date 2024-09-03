84b7c1da79bec4fe1fdeda95ee6e58a84b474f39
Yuan Cao <zhangfei5056@126.com>
2024-09-03 15:01:39 +0800

    init project

---
+++ LearnWidget/LearnWidget/LearnWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnWidgetApp.swift
+//  LearnWidget
+//
+//  Created by Yuan Cao on 2024/9/3.
+//
+
+import SwiftUI
+
+@main
+struct LearnWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
