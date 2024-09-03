f979ff965148b10820c585ed7786a21b5880d7b2
Yuan Cao <zhangfei5056@126.com>
2024-09-03 15:11:42 +0800

    add MyWidget Target as extension and iOS17.5 supported

---
+++ LearnWidget/MyWidget/MyWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MyWidgetBundle.swift
+//  MyWidget
+//
+//  Created by Yuan Cao on 2024/9/3.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MyWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        MyWidget()
+    }
+}
